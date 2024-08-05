a0db7891fcf792dd7d8997a31f916e0b126304c9
Paul Kukowski <kukowskipaul@gmail.com>
2024-08-05 10:41:27 +0200

    Fixed pairing algorithm

--- mtgcube/tournaments/services.py
+++ mtgcube/tournaments/services.py
@@ -1,12 +1,14 @@
 import random
 
-from .models import Game, Enrollment, Round
+import networkx as nx
+
+from .models import Game, Enrollment, Round, Draft, Phase
 from . import queries
 
 
 def seat_draft(draft):
     reset_draft_scores(draft)
-    players = list(draft.enrollments.all())
+    players = list(draft.enrollments.filter(dropped=False))
 
     random.shuffle(players)
     for idx, player in enumerate(players):
@@ -15,6 +17,119 @@ def seat_draft(draft):
     draft.started = True
     draft.save()
 
+
+def pair_round_new(draft):
+    current_round = queries.current_round(draft, force_update=True)
+
+    if current_round:
+        if current_round.round_idx == draft.round_number:
+            return IndexError("Draft already has all rounds.")
+        new_rd = Round(draft=draft, round_idx=current_round.round_idx + 1)
+        new_rd.save()
+    else:
+        new_rd = Round(draft=draft, round_idx=1)
+        new_rd.save()
+
+    players = list(Enrollment.objects.filter(draft=draft, dropped=False))
+
+    # Clear old round pairings
+    for player in players:
+        player.paired = False
+        player.bye_this_round = False
+        player.save()
+
+    startingTable = 1 # TODO
+    openTable = startingTable
+
+    # Contains lists of players sorted by how many points they currently have
+    pointLists = {}
+
+    # Contains a list of points in the event from high to low
+    pointTotals = []
+
+    # Counts our groupings for each point amount
+    countPoints = {}
+
+    # Add all players to pointLists
+    for player in players:
+        # If this point amount isn't in the list, add it
+        if "%s_1" % player.draft_score not in pointLists:
+            pointLists["%s_1" % player.draft_score] = []
+            countPoints[player.draft_score] = 1
+
+        # Breakers the players into groups of their current points up to the max group allowed.
+        # Smaller groups mean faster calculations
+        if len(pointLists["%s_%s" % (player.draft_score, countPoints[player.draft_score])]) > 25:
+            countPoints[player.draft_score] += 1
+            pointLists["%s_%s" % (player.draft_score, countPoints[player.draft_score])] = []
+
+        # Add our player to the correct group
+        pointLists["%s_%s" % (player.draft_score, countPoints[player.draft_score])].append(player)
+
+    # Add all points in use to pointTotals
+    for points in pointLists:
+        pointTotals.append(points)
+
+    # Sort our point groups based on points
+    pointTotals.sort(reverse=True, key=lambda s: int(s.split('_')[0]))
+
+    print("Point totals after sorting high to low are: %s" % pointTotals, 3)
+
+    # Actually pair the players utilizing graph theory networkx
+    for points in pointTotals:
+        print(points, 5)
+
+        # Create the graph object and add all players to it
+        bracketGraph = nx.Graph()
+        bracketGraph.add_nodes_from(pointLists[points])
+
+        print(pointLists[points], 5)
+        print(bracketGraph.nodes(), 5)
+
+        # Create edges between all players in the graph who haven't already played
+        for player in bracketGraph.nodes():
+            for opponent in bracketGraph.nodes():
+                if opponent not in player.pairings.all() and player != opponent:
+                    # Weight edges randomly between 1 and 9 to ensure pairings are not always the same with
+                    # the same list of players
+                    wgt = random.randint(1, 9)
+                    # If a player has more points, weigh them the highest, so they get paired first
+                    if player.draft_score > int(points.split('_')[0]) or \
+                            opponent.draft_score > int(points.split('_')[0]):
+                        wgt = 10
+                    # Create edge
+                    bracketGraph.add_edge(player, opponent, weight=wgt)
+
+        # Generate pairings from the created graph
+        pairings = dict(nx.max_weight_matching(bracketGraph))
+
+        print(pairings, 3)
+
+        # Actually pair the players based on the matching we found
+        for p in pairings:
+            if p in pointLists[points]:
+                pair(new_rd, p, pairings[p], openTable)
+                openTable += 1
+                pointLists[points].remove(p)
+                pointLists[points].remove(pairings[p])
+
+        print(pointLists[points], 5)
+
+        # Check if we have an odd man out that we need to pair down
+        if len(pointLists[points]) > 0:
+            # Check to make sure we aren't at the last player in the event
+            print("Player %s left in %s. The index is %s and the length of totals is %s" % (
+                pointLists[points][0], points, pointTotals.index(points), len(pointTotals)), 3)
+            if pointTotals.index(points) + 1 == len(pointTotals):
+                while len(pointLists[points]) > 0:
+                    # If they are the last player give them a bye
+                    assign_bye(pointLists[points].pop(0))
+            else:
+                # Add our player to the next point group down
+                nextPoints = pointTotals[pointTotals.index(points) + 1]
+
+                while len(pointLists[points]) > 0:
+                    pointLists[nextPoints].append(pointLists[points].pop(0))
 
 def pair_round(draft):
     """
@@ -48,7 +163,7 @@ def pair_round(draft):
         new_rd = Round(draft=draft, round_idx=1)
         new_rd.save()
 
-    players = list(Enrollment.objects.filter(draft=draft))
+    players = list(Enrollment.objects.filter(draft=draft, dropped=False))
 
     random.shuffle(players)
 
@@ -167,12 +282,17 @@ def update_draft_tiebreakers(draft):
         player.draft_ogw = 0.0
         player.save()
     for player in players:
-        for opponent in player.pairings.all():
-            player.draft_omw += opponent.draft_pmw
-            player.draft_ogw += opponent.draft_pgw
-        player.draft_omw = max(round(player.draft_omw / current_round, 2), 0.33)
-        player.draft_ogw = max(round(player.draft_ogw / current_round, 2), 0.33)
-        player.save()
+        if len(player.pairings.all()) == 0:
+            player.draft_omw = 1.0
+            player.draft_ogw = 1.0
+            player.save()
+        else:
+            for opponent in player.pairings.all():
+                player.draft_omw += opponent.draft_pmw
+                player.draft_ogw += opponent.draft_pgw
+            player.draft_omw = max(round(player.draft_omw / current_round, 2), 0.33)
+            player.draft_ogw = max(round(player.draft_ogw / current_round, 2), 0.33)
+            player.save()
 
 
 def update_tournament_tiebreakers(tournament):
@@ -257,6 +377,7 @@ def reset_draft_scores(draft):
 def clear_histories(draft):
     draft.seated = False
     draft.started = False
+    draft.finished = False
     draft.save()
 
     players = draft.enrollments.all()
@@ -294,3 +415,19 @@ def clear_histories(draft):
 
     for rd in rounds:
         rd.delete()
+
+def reset_tournament(tournament):
+
+    tournament.current_round = 1
+    tournament.save()
+
+    phases = Phase.objects.filter(tournament=tournament)
+    drafts = Draft.objects.filter(phase__tournament=tournament)
+
+    for p in phases:
+        p.started = False
+        p.finished = False
+        p.save()
+    
+    for d in drafts:
+        clear_histories(d)
