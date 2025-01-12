2d67797882b763714e8a67d8215a5c2dbadab017
Paul Kukowski <paul.kukowski@webid-solutions.de>
2025-01-12 16:42:37 +0100

    Re-added email-requirement and add warning for password reset without email

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,8 +15,8 @@ env = environ.Env(DEBUG=(bool, False))
 
 READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=False)
 if READ_DOT_ENV_FILE:
-  # OS environment variables take precedence over variables from .env
-  env.read_env(str(ROOT_DIR / ".env"))
+    # OS environment variables take precedence over variables from .env
+    env.read_env(str(ROOT_DIR / ".env"))
 
 # GENERAL
 # ------------------------------------------------------------------------------
@@ -41,8 +41,8 @@ USE_TZ = True
 LOCALE_PATHS = [str(ROOT_DIR / "locale")]
 
 LANGUAGES = (
-  ("en", ("English")),
-  ("de", ("German")),
+    ("en", ("English")),
+    ("de", ("German")),
 )
 
 # DATABASES
@@ -50,10 +50,10 @@ LANGUAGES = (
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-  "default": {
-    "ENGINE": "django.db.backends.sqlite3",
-    "NAME": ROOT_DIR / "db.sqlite3",
-  }
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ROOT_DIR / "db.sqlite3",
+    }
 }
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
@@ -66,37 +66,37 @@ ROOT_URLCONF = "config.urls"
 WSGI_APPLICATION = "config.wsgi.application"
 
 ACCOUNT_FORMS = {
-  "signup": "mtgcube.users.forms.CustomSignupForm",
+    "signup": "mtgcube.users.forms.CustomSignupForm",
 }
 
 # APPS
 # ------------------------------------------------------------------------------
 DJANGO_APPS = [
-  "django.contrib.auth",
-  "django.contrib.contenttypes",
-  "django.contrib.sessions",
-  "django.contrib.sites",
-  "django.contrib.messages",
-  "django.contrib.staticfiles",
-  "django.contrib.admin",
-  "django.forms",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.admin",
+    "django.forms",
 ]
 THIRD_PARTY_APPS = [
-  "corsheaders",
-  "crispy_forms",
-  "crispy_bootstrap5",
-  "allauth",
-  "allauth.account",
-  "allauth.socialaccount",
-  "allauth.socialaccount.providers.google",
-  "allauth.socialaccount.providers.facebook",
-  "dynamic_breadcrumbs",
-  "termsandconditions",
+    "corsheaders",
+    "crispy_forms",
+    "crispy_bootstrap5",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.google",
+    "allauth.socialaccount.providers.facebook",
+    "dynamic_breadcrumbs",
+    "termsandconditions",
 ]
 
 LOCAL_APPS = [
-  "tournaments",
-  "mtgcube.users.apps.UsersConfig",
+    "tournaments",
+    "mtgcube.users.apps.UsersConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
@@ -110,8 +110,8 @@ MIGRATION_MODULES = {"sites": "mtgcube.contrib.sites.migrations"}
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#authentication-backends
 AUTHENTICATION_BACKENDS = [
-  "django.contrib.auth.backends.ModelBackend",
-  "allauth.account.auth_backends.AuthenticationBackend",
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
 AUTH_USER_MODEL = "users.User"
@@ -124,36 +124,38 @@ LOGIN_URL = "account_login"
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#password-hashers
 PASSWORD_HASHERS = [
-  # https://docs.djangoproject.com/en/dev/topics/auth/passwords/#using-argon2-with-django
-  "django.contrib.auth.hashers.Argon2PasswordHasher",
-  "django.contrib.auth.hashers.PBKDF2PasswordHasher",
-  "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
-  "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    # https://docs.djangoproject.com/en/dev/topics/auth/passwords/#using-argon2-with-django
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
-  {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
-  {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
-  {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
-  {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 # MIDDLEWARE
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#middleware
 MIDDLEWARE = [
-  "corsheaders.middleware.CorsMiddleware",
-  "django.middleware.security.SecurityMiddleware",
-  "django.contrib.sessions.middleware.SessionMiddleware",
-  "django.middleware.common.CommonMiddleware",
-  "django.middleware.locale.LocaleMiddleware",
-  "django.middleware.csrf.CsrfViewMiddleware",
-  "django.contrib.auth.middleware.AuthenticationMiddleware",
-  "termsandconditions.middleware.TermsAndConditionsRedirectMiddleware",
-  "django.contrib.messages.middleware.MessageMiddleware",
-  "django.middleware.common.BrokenLinkEmailsMiddleware",
-  "django.middleware.clickjacking.XFrameOptionsMiddleware",
-  "allauth.account.middleware.AccountMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "termsandconditions.middleware.TermsAndConditionsRedirectMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.common.BrokenLinkEmailsMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 
@@ -167,8 +169,8 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [str(APPS_DIR / "static")]
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = [
-  "django.contrib.staticfiles.finders.FileSystemFinder",
-  "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
 # MEDIA
@@ -182,33 +184,33 @@ MEDIA_URL = "/media/"
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#templates
 TEMPLATES = [
-  {
-    # https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-TEMPLATES-BACKEND
-    "BACKEND": "django.template.backends.django.DjangoTemplates",
-    # https://docs.djangoproject.com/en/dev/ref/settings/#template-dirs
-    "DIRS": [str(APPS_DIR / "templates")],
-    "OPTIONS": {
-      # https://docs.djangoproject.com/en/dev/ref/settings/#template-loaders
-      # https://docs.djangoproject.com/en/dev/ref/templates/api/#loader-types
-      "loaders": [
-        "django.template.loaders.filesystem.Loader",
-        "django.template.loaders.app_directories.Loader",
-      ],
-      # https://docs.djangoproject.com/en/dev/ref/settings/#template-context-processors
-      "context_processors": [
-        "django.template.context_processors.debug",
-        "django.template.context_processors.request",
-        "django.contrib.auth.context_processors.auth",
-        "django.template.context_processors.i18n",
-        "django.template.context_processors.media",
-        "django.template.context_processors.static",
-        "django.template.context_processors.tz",
-        "django.contrib.messages.context_processors.messages",
-        "mtgcube.utils.context_processors.settings_context",
-        "dynamic_breadcrumbs.context_processors.breadcrumbs",
-      ],
-    },
-  }
+    {
+        # https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-TEMPLATES-BACKEND
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        # https://docs.djangoproject.com/en/dev/ref/settings/#template-dirs
+        "DIRS": [str(APPS_DIR / "templates")],
+        "OPTIONS": {
+            # https://docs.djangoproject.com/en/dev/ref/settings/#template-loaders
+            # https://docs.djangoproject.com/en/dev/ref/templates/api/#loader-types
+            "loaders": [
+                "django.template.loaders.filesystem.Loader",
+                "django.template.loaders.app_directories.Loader",
+            ],
+            # https://docs.djangoproject.com/en/dev/ref/settings/#template-context-processors
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                "django.contrib.messages.context_processors.messages",
+                "mtgcube.utils.context_processors.settings_context",
+                "dynamic_breadcrumbs.context_processors.breadcrumbs",
+            ],
+        },
+    }
 ]
 
 DYNAMIC_BREADCRUMBS_PATH_MAX_COMPONENT_LENGTH = 100
@@ -220,12 +222,12 @@ FORM_RENDERER = "django.forms.renderers.TemplatesSetting"
 # http://django-crispy-forms.readthedocs.io/en/latest/install.html#template-packs
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 CRISPY_ALLOWED_TEMPLATE_PACKS = (
-  "bootstrap",
-  "uni_form",
-  "bootstrap3",
-  "bootstrap4",
-  "bootstrap5",
-  "semantic-ui",
+    "bootstrap",
+    "uni_form",
+    "bootstrap3",
+    "bootstrap4",
+    "bootstrap5",
+    "semantic-ui",
 )
 
 # FIXTURES
@@ -248,7 +250,7 @@ X_FRAME_OPTIONS = "DENY"
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 EMAIL_BACKEND = env(
-  "DJANGO_EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend"
+    "DJANGO_EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend"
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-timeout
 EMAIL_TIMEOUT = 5
@@ -264,36 +266,32 @@ ADMIN_URL = "admin/"
 # See https://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
-  "version": 1,
-  "disable_existing_loggers": False,
-  "formatters": {
-    "verbose": {
-      "format": "%(levelname)s %(asctime)s %(module)s "
-      "%(process)d %(thread)d %(message)s"
-    }
-  },
-  "handlers": {
-    "console": {
-      "level": "DEBUG",
-      "class": "logging.StreamHandler",
-      "formatter": "verbose",
-    }
-  },
-  "root": {"level": "INFO", "handlers": ["console"]},
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(asctime)s %(module)s "
+            "%(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        }
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
 }
 
 # django-allauth
+# https://django-allauth.readthedocs.io/en/latest/configuration.html
 # ------------------------------------------------------------------------------
 ACCOUNT_ALLOW_REGISTRATION = env.bool("DJANGO_ACCOUNT_ALLOW_REGISTRATION", True)
-# https://django-allauth.readthedocs.io/en/latest/configuration.html
-ACCOUNT_AUTHENTICATION_METHOD = "username"
-# https://django-allauth.readthedocs.io/en/latest/configuration.html
-ACCOUNT_EMAIL_REQUIRED = False
-# https://django-allauth.readthedocs.io/en/latest/configuration.html
+ACCOUNT_AUTHENTICATION_METHOD = "username_email"
+ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = "none"
-# https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_ADAPTER = "users.adapters.AccountAdapter"
-# https://django-allauth.readthedocs.io/en/latest/configuration.html
 SOCIALACCOUNT_ADAPTER = "users.adapters.SocialAccountAdapter"
 SOCIALACCOUNT_AUTO_SIGNUP = False
 SOCIALACCOUNT_EMAIL_REQUIRED = False
@@ -301,14 +299,6 @@ SECURE_REFERRER_POLICY = "strict-origin-when-cross-origin"
 SECURE_CROSS_ORIGIN_OPENER_POLICY = "same-origin-allow-popups"
 
 CORS_ALLOW_ALL_ORIGINS = True
-""" CORS_ALLOWED_ORIGINS = [
-    "https://vault.mtg-cube.de",
-    "https://*.mtg-cube.de",
-    "https://mtg-cube.de",
-    "https://storage.googleapis.com",
-    "http://localhost",
-    "http://localhost:8080"
-] """
 
 SOCIALACCOUNT_LOGIN_ON_GET = True
 
@@ -317,17 +307,17 @@ CACHE_MIDDLEWARE_KEY_PREFIX = "mtg"
 
 # ------------------------------------------------------------------------------
 SOCIALACCOUNT_PROVIDERS = {
-  "google": {
-    "SCOPE": [
-      "profile",
-      "email",
-    ],
-    "AUTH_PARAMS": {
-      "access_type": "online",
-    },
-    "OAUTH_PKCE_ENABLED": True,
-    "FETCH_USERINFO": True,
-  }
+    "google": {
+        "SCOPE": [
+            "profile",
+            "email",
+        ],
+        "AUTH_PARAMS": {
+            "access_type": "online",
+        },
+        "OAUTH_PKCE_ENABLED": True,
+        "FETCH_USERINFO": True,
+    }
 }
 
 SOCIALACCOUNT_FORMS = {"signup": "users.forms.CustomSocialSignupForm"}
