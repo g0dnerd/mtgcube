ad92d54749d697a565df31bb7db95b2e6dd5c719
Paul Kukowski <paul.kukowski@webid-solutions.de>
2025-01-12 17:46:16 +0100

    Address #3

--- mtgcube/tournaments/forms.py
+++ mtgcube/tournaments/forms.py
@@ -5,26 +5,27 @@ from django.utils.translation import gettext_lazy as _
 
 
 class ImageForm(forms.ModelForm):
-  class Meta:
-    model = Image
-    label = _("Image")
-    fields = ["image"]
+    class Meta:
+        model = Image
+        label = _("Image")
+        fields = ["image"]
 
-  image = forms.ImageField()
+    image = forms.ImageField()
 
 
 class ReportResultForm(forms.Form):
-  match_id = forms.CharField(widget=forms.HiddenInput())
-  player1_wins = forms.ChoiceField(choices=[(i, str(i)) for i in range(3)])
-  player2_wins = forms.ChoiceField(choices=[(i, str(i)) for i in range(3)])
+    match_id = forms.CharField(widget=forms.HiddenInput())
+    player1_wins = forms.ChoiceField(choices=[(i, str(i)) for i in range(3)])
+    player2_wins = forms.ChoiceField(choices=[(i, str(i)) for i in range(3)])
 
-  def clean(self):
-    cleaned_data = super().clean()
-    p1w = cleaned_data.get("player1_wins")
-    p2w = cleaned_data.get("player2_wins")
-    if int(p1w) + int(p2w) > 3:
-      raise forms.ValidationError(_("Total wins cannot be greater than 3."))
+    def clean(self):
+        cleaned_data = super().clean()
+        p1w = cleaned_data.get("player1_wins")
+        p2w = cleaned_data.get("player2_wins")
+
+        if int(p1w) + int(p2w) > 3:
+            raise forms.ValidationError(_("Total wins cannot be greater than 3."))
 
 
 class ConfirmResultForm(forms.Form):
-  match_id = forms.CharField(widget=forms.HiddenInput())
+    confirm_match_id = forms.CharField(widget=forms.HiddenInput())
--- mtgcube/tournaments/views/form_views.py
+++ mtgcube/tournaments/views/form_views.py
@@ -13,262 +13,281 @@ User = get_user_model()
 
 
 class AdminDataMixin(UserPassesTestMixin):
-  def test_func(self):
-    return self.request.user.is_superuser
+    def test_func(self):
+        return self.request.user.is_superuser
 
-  def handle_no_permission(self):
-    return JsonResponse({"error": "Insufficient authentication level"}, status=403)
+    def handle_no_permission(self):
+        return JsonResponse({"error": "Insufficient authentication level"}, status=403)
 
 
 class ConfirmResultView(FormView, LoginRequiredMixin):
-  form_class = ConfirmResultForm
-  template_name = "tournaments/current_draft.html"
+    form_class = ConfirmResultForm
+    template_name = "tournaments/current_draft.html"
 
-  def get_success_url(self):
-    return reverse_lazy("tournaments:draft_dashboard", kwargs=self.kwargs)
+    def get_success_url(self):
+        return reverse_lazy("tournaments:draft_dashboard", kwargs=self.kwargs)
 
-  def form_valid(self, form):
-    match_id = form.cleaned_data["confirm_match_id"]
+    def form_valid(self, form):
+        match_id = form.cleaned_data["confirm_match_id"]
+        match = queries.get_match(match_id)
 
-    match = queries.get_match(match_id)
-
-    if match:
-      services.finish_match(match)
-      return redirect(self.get_success_url())
-    else:
-      return JsonResponse({"error": "Game not found"}, status=404)
+        if match:
+            services.finish_match(match)
+            return redirect(self.get_success_url())
+        else:
+            return JsonResponse({"error": "Game not found"}, status=404)
 
 
 class PlayerReportResultView(FormView, LoginRequiredMixin):
-  form_class = ReportResultForm
-  template_name = "tournaments/current_draft.html"
+    form_class = ReportResultForm
+    template_name = "tournaments/current_draft.html"
 
-  def get_success_url(self):
-    return reverse_lazy("tournaments:draft_dashboard", kwargs=self.kwargs)
+    def get_success_url(self):
+        return reverse_lazy("tournaments:draft_dashboard", kwargs=self.kwargs)
 
-  def form_valid(self, form):
-    match_id = form.cleaned_data["match_id"]
-    player1_wins = form.cleaned_data["player1_wins"]
-    player2_wins = form.cleaned_data["player2_wins"]
-    player = queries.get_player(self.request.user)
-    match = queries.get_match(match_id)
-    services.report_result(
-      match,
-      player1_wins,
-      player2_wins,
-      reporting_player=player,
-      admin=False,
-    )
-    return redirect(self.get_success_url())
+    def form_valid(self, form):
+        match_id = form.cleaned_data["match_id"]
+        player1_wins = form.cleaned_data["player1_wins"]
+        player2_wins = form.cleaned_data["player2_wins"]
 
-  def form_invalid(self, form):
-    messages.error(self.request, "Error: Please enter a valid game result.")
-    return redirect(reverse_lazy("tournaments:draft_dashboard", kwargs=self.kwargs))
+        player = queries.get_player(self.request.user)
+        match = queries.get_match(match_id)
+        services.report_result(
+            match,
+            player1_wins,
+            player2_wins,
+            reporting_player=player,
+            admin=False,
+        )
+        return redirect(self.get_success_url())
+
+    def form_invalid(self, form):
+        messages.error(self.request, "Error: Please enter a valid game result.")
+        return redirect(reverse_lazy("tournaments:draft_dashboard", kwargs=self.kwargs))
 
 
 class AdminReportResultView(FormView, AdminDataMixin):
-  form_class = ReportResultForm
-  template_name = "tournaments/admin_draft_dashboard.html"
+    form_class = ReportResultForm
+    template_name = "tournaments/admin_draft_dashboard.html"
 
-  def get_success_url(self):
-    return reverse_lazy(
-      "tournaments:admin_draft_dashboard",
-      kwargs={"slug": self.kwargs["slug"], "draft_slug": self.kwargs["draft_slug"]},
-    )
+    def get_success_url(self):
+        return reverse_lazy(
+            "tournaments:admin_draft_dashboard",
+            kwargs={
+                "slug": self.kwargs["slug"],
+                "draft_slug": self.kwargs["draft_slug"],
+            },
+        )
 
-  def form_valid(self, form):
-    match_id = form.cleaned_data["match_id"]
-    player1_wins = form.cleaned_data["player1_wins"]
-    player2_wins = form.cleaned_data["player2_wins"]
-    match = queries.get_match(match_id)
-    services.report_result(
-      match,
-      player1_wins,
-      player2_wins,
-      reporting_player=None,
-      admin=True,
-    )
-    services.finish_match(match)
-    return super().form_valid(form)
+    def form_valid(self, form):
+        match_id = form.cleaned_data["match_id"]
+        player1_wins = form.cleaned_data["player1_wins"]
+        player2_wins = form.cleaned_data["player2_wins"]
+        match = queries.get_match(match_id)
+        services.report_result(
+            match,
+            player1_wins,
+            player2_wins,
+            reporting_player=None,
+            admin=True,
+        )
+        services.finish_match(match)
+        return super().form_valid(form)
 
 
 class AdminConfirmResultView(FormView, LoginRequiredMixin):
-  form_class = ConfirmResultForm
-  template_name = "tournaments/admin_draft_dashboard.html"
+    form_class = ConfirmResultForm
+    template_name = "tournaments/admin_draft_dashboard.html"
 
-  def get_success_url(self):
-    return reverse_lazy(
-      "tournaments:admin_draft_dashboard",
-      kwargs={"slug": self.kwargs["slug"], "draft_slug": self.kwargs["draft_slug"]},
-    )
+    def get_success_url(self):
+        return reverse_lazy(
+            "tournaments:admin_draft_dashboard",
+            kwargs={
+                "slug": self.kwargs["slug"],
+                "draft_slug": self.kwargs["draft_slug"],
+            },
+        )
 
-  def form_valid(self, form):
-    match_id = form.cleaned_data["match_id"]
+    def form_valid(self, form):
+        match_id = form.cleaned_data["match_id"]
 
-    match = queries.get_match(match_id)
+        match = queries.get_match(match_id)
 
-    if match:
-      services.finish_match(match)
-      return redirect(self.get_success_url())
-    else:
-      return JsonResponse({"error": "Game not found"}, status=404)
+        if match:
+            services.finish_match(match)
+            return redirect(self.get_success_url())
+        else:
+            return JsonResponse({"error": "Game not found"}, status=404)
 
 
 class SeatDraftView(FormView, AdminDataMixin):
-  template_name = "tournaments/admin_draft_dashboard.html"
+    template_name = "tournaments/admin_draft_dashboard.html"
 
-  def get_success_url(self):
-    return reverse_lazy(
-      "tournaments:admin_draft_dashboard",
-      kwargs={"slug": self.kwargs["slug"], "draft_slug": self.kwargs["draft_slug"]},
-    )
+    def get_success_url(self):
+        return reverse_lazy(
+            "tournaments:admin_draft_dashboard",
+            kwargs={
+                "slug": self.kwargs["slug"],
+                "draft_slug": self.kwargs["draft_slug"],
+            },
+        )
 
-  def post(self, request, *args, **kwargs):
-    slug = kwargs.get("draft_slug")
-    draft = queries.get_draft(slug=slug)
-    if not draft.seated:
-      services.seat_draft(draft)
-    return redirect(self.get_success_url())
+    def post(self, request, *args, **kwargs):
+        slug = kwargs.get("draft_slug")
+        draft = queries.get_draft(slug=slug)
+        if not draft.seated:
+            services.seat_draft(draft)
+        return redirect(self.get_success_url())
 
 
 class PairRoundView(FormView, AdminDataMixin):
-  template_name = "tournaments/admin_draft_dashboard.html"
+    template_name = "tournaments/admin_draft_dashboard.html"
 
-  def get_success_url(self):
-    return reverse_lazy(
-      "tournaments:admin_draft_dashboard",
-      kwargs={"slug": self.kwargs["slug"], "draft_slug": self.kwargs["draft_slug"]},
-    )
+    def get_success_url(self):
+        return reverse_lazy(
+            "tournaments:admin_draft_dashboard",
+            kwargs={
+                "slug": self.kwargs["slug"],
+                "draft_slug": self.kwargs["draft_slug"],
+            },
+        )
 
-  def post(self, request, *args, **kwargs):
-    slug = kwargs.get("draft_slug")
-    draft = queries.get_draft(slug=slug)
-    services.pair_round_new(draft)
-    return redirect(self.get_success_url())
+    def post(self, request, *args, **kwargs):
+        slug = kwargs.get("draft_slug")
+        draft = queries.get_draft(slug=slug)
+        services.pair_round_new(draft)
+        return redirect(self.get_success_url())
 
 
 class FinishRoundView(FormView, AdminDataMixin):
-  template_name = "tournaments/admin_draft_dashboard.html"
+    template_name = "tournaments/admin_draft_dashboard.html"
 
-  def get_success_url(self):
-    return reverse_lazy(
-      "tournaments:admin_draft_dashboard",
-      kwargs={"slug": self.kwargs["slug"], "draft_slug": self.kwargs["draft_slug"]},
-    )
+    def get_success_url(self):
+        return reverse_lazy(
+            "tournaments:admin_draft_dashboard",
+            kwargs={
+                "slug": self.kwargs["slug"],
+                "draft_slug": self.kwargs["draft_slug"],
+            },
+        )
 
-  def post(self, request, *args, **kwargs):
-    slug = kwargs.get("draft_slug")
-    draft = queries.get_draft(slug=slug)
-    current_rd = queries.current_round(draft, force_update=True)
-    services.finish_draft_round(current_rd)
-    return redirect(self.get_success_url())
+    def post(self, request, *args, **kwargs):
+        slug = kwargs.get("draft_slug")
+        draft = queries.get_draft(slug=slug)
+        current_rd = queries.current_round(draft, force_update=True)
+        services.finish_draft_round(current_rd)
+        return redirect(self.get_success_url())
 
 
 class ResetDraftView(FormView, AdminDataMixin):
-  template_name = "tournaments/admin_draft_dashboard.html"
+    template_name = "tournaments/admin_draft_dashboard.html"
 
-  def get_success_url(self):
-    return reverse_lazy(
-      "tournaments:admin_draft_dashboard",
-      kwargs={"slug": self.kwargs["slug"], "draft_slug": self.kwargs["draft_slug"]},
-    )
+    def get_success_url(self):
+        return reverse_lazy(
+            "tournaments:admin_draft_dashboard",
+            kwargs={
+                "slug": self.kwargs["slug"],
+                "draft_slug": self.kwargs["draft_slug"],
+            },
+        )
 
-  def post(self, request, *args, **kwargs):
-    slug = kwargs.get("draft_slug")
-    draft = queries.get_draft(slug=slug)
-    services.clear_histories(draft)
-    return redirect(self.get_success_url())
+    def post(self, request, *args, **kwargs):
+        slug = kwargs.get("draft_slug")
+        draft = queries.get_draft(slug=slug)
+        services.clear_histories(draft)
+        return redirect(self.get_success_url())
 
 
 class FinishEventRoundView(FormView, AdminDataMixin):
-  template_name = "tournaments/admin_dashboard.html"
+    template_name = "tournaments/admin_dashboard.html"
 
-  def get_success_url(self):
-    return reverse_lazy("tournaments:admin_dashboard", kwargs=self.kwargs)
+    def get_success_url(self):
+        return reverse_lazy("tournaments:admin_dashboard", kwargs=self.kwargs)
 
-  def post(self, request, *args, **kwargs):
-    event_id = request.POST.get("finish-event-round")
-    event = queries.get_tournament(id=event_id)
+    def post(self, request, *args, **kwargs):
+        event_id = request.POST.get("finish-event-round")
+        event = queries.get_tournament(id=event_id)
 
-    services.finish_event_round(event)
+        services.finish_event_round(event)
 
-    return redirect(self.get_success_url())
+        return redirect(self.get_success_url())
 
 
 class StartPhaseView(FormView, AdminDataMixin):
-  template_name = "tournaments/admin_dashboard.html"
+    template_name = "tournaments/admin_dashboard.html"
 
-  def get_success_url(self):
-    return reverse_lazy("tournaments:admin_dashboard", kwargs=self.kwargs)
+    def get_success_url(self):
+        return reverse_lazy("tournaments:admin_dashboard", kwargs=self.kwargs)
 
-  def post(self, request, *args, **kwargs):
-    tournament_id = request.POST.get("start-phase")
-    tournament = queries.get_tournament(id=tournament_id)
+    def post(self, request, *args, **kwargs):
+        tournament_id = request.POST.get("start-phase")
+        tournament = queries.get_tournament(id=tournament_id)
 
-    phase_idx = tournament.current_round // 3 + 1
-    phase = queries.get_phase_by_index(tournament, phase_idx)
+        phase_idx = tournament.current_round // 3 + 1
+        phase = queries.get_phase_by_index(tournament, phase_idx)
 
-    phase.started = True
-    phase.save()
+        phase.started = True
+        phase.save()
 
-    return redirect(self.get_success_url())
+        return redirect(self.get_success_url())
 
 
 class ResetEventView(FormView, AdminDataMixin):
-  template_name = "tournaments/admin_dashboard.html"
+    template_name = "tournaments/admin_dashboard.html"
 
-  def get_success_url(self):
-    return reverse_lazy(
-      "tournaments:admin_dashboard", kwargs={"slug": self.kwargs["slug"]}
-    )
+    def get_success_url(self):
+        return reverse_lazy(
+            "tournaments:admin_dashboard", kwargs={"slug": self.kwargs["slug"]}
+        )
 
-  def post(self, request, *args, **kwargs):
-    event_id = request.POST.get("reset-event")
-    event = queries.get_tournament(id=event_id)
+    def post(self, request, *args, **kwargs):
+        event_id = request.POST.get("reset-event")
+        event = queries.get_tournament(id=event_id)
 
-    services.reset_tournament(event)
+        services.reset_tournament(event)
 
-    return redirect(self.get_success_url())
+        return redirect(self.get_success_url())
 
 
 class EventEnrollView(FormView, LoginRequiredMixin):
-  template_name = "tournaments/tournament_list.html"
-  success_url = reverse_lazy("tournaments:index")
+    template_name = "tournaments/tournament_list.html"
+    success_url = reverse_lazy("tournaments:index")
 
-  def post(self, request, *args, **kwargs):
-    event_id = request.POST.get("event-id")
-    tournament = queries.get_tournament(id=int(event_id))
-    try:
-      services.enroll_for_event(request.user, tournament)
-    except ValueError as e:
-      messages.error(request, str(e))
-      return redirect("tournaments:registration")
+    def post(self, request, *args, **kwargs):
+        event_id = request.POST.get("event-id")
+        tournament = queries.get_tournament(id=int(event_id))
+        try:
+            services.enroll_for_event(request.user, tournament)
+        except ValueError as e:
+            messages.error(request, str(e))
+            return redirect("tournaments:registration")
 
-    messages.success(request, f"You successfully registered for {tournament.name}!")
+        messages.success(request, f"You successfully registered for {tournament.name}!")
 
-    return redirect(self.get_success_url())
+        return redirect(self.get_success_url())
 
 
 class AdminEnrollUserView(FormView, AdminDataMixin):
-  template_name = "tournaments/admin_tournament_players.html"
+    template_name = "tournaments/admin_tournament_players.html"
 
-  def get_success_url(self):
-    return reverse_lazy(
-      "tournaments:admin_player_list", kwargs={"slug": self.kwargs["slug"]}
-    )
+    def get_success_url(self):
+        return reverse_lazy(
+            "tournaments:admin_player_list", kwargs={"slug": self.kwargs["slug"]}
+        )
 
-  def post(self, request, *args, **kwargs):
-    user_id = request.POST.get("user-id")
-    user = User.objects.get(id=int(user_id))
-    tournament_slug = kwargs["slug"]
-    tournament = queries.get_tournament(slug=tournament_slug)
+    def post(self, request, *args, **kwargs):
+        user_id = request.POST.get("user-id")
+        user = User.objects.get(id=int(user_id))
+        tournament_slug = kwargs["slug"]
+        tournament = queries.get_tournament(slug=tournament_slug)
 
-    try:
-      services.enroll_for_event(user, tournament)
-      messages.success(
-        request, f"{user.name} was successfully registered for {tournament.name}!"
-      )
-    except ValueError as e:
-      messages.error(request, f"Error: {e}")
+        try:
+            services.enroll_for_event(user, tournament)
+            messages.success(
+                request,
+                f"{user.name} was successfully registered for {tournament.name}!",
+            )
+        except ValueError as e:
+            messages.error(request, f"Error: {e}")
 
-    return redirect(self.get_success_url())
+        return redirect(self.get_success_url())
